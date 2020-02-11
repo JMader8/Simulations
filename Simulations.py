56ff6ff6efb770403b70c762a8db4d21debdda2e
unknown <JMader8>
2020-02-11 08:50:12 -0600

    Generates random number, cannot assign percentages though.

--- Simulations.py
+++ Simulations.py
@@ -1,15 +1,33 @@
 import random
 
-rollcount = float(input("How many dice rolls ya want?"))
+rollcount = int(input("How many dice rolls ya want?"))
 print(f"{rollcount} rolls. Rolling now.")
 
-roll = random(1,6)
+
 
 i=0
 
+
 while(i<rollcount):
-    i +=1
-    print(i)
+
     
+    
+    i +=1
+    
+    num1=1
+    num2=2
+    num3=3
+    num4=4
+    num5=5
+    num6=6
+    st=random.randint(num1,num6)
+    
+    print(i, st )
+
+    print("Next Roll: ")
+    
+
+
+
 
 
